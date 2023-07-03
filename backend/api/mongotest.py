206846477afa0ad6b4e26f33541768869fc32dd2
Manoj Narender <85360326+theman-oj10@users.noreply.github.com>
2023-07-04 01:21:34 +0800

    Update mongotest.py

--- backend/api/mongotest.py
+++ backend/api/mongotest.py
@@ -1,7 +1,7 @@
 from pymongo import MongoClient
 
 # Setup MongoDB
-cluster = MongoClient("mongodb+srv://manojnarender:ManojDaBeast7@cluster0.jhnmbg1.mongodb.net/")
+cluster = MongoClient("mongodb+srv://manojnarender:<Password>7@cluster0.jhnmbg1.mongodb.net/")
 db = cluster['ToDoApp']
 collection = db["toDoList"]
 
